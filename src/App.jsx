37babe4b92a41c287d563a7c93b7ad3620f7d634
Gustavo Lastra <lastraemiliano1@gmail.com>
2025-01-21 08:47:31 -0600

    paginator, divs con css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Paginator from './components/paginator'
+ 
 
 export default function App() {
   return (
+
+
+    <>
     <div className='bg-slate-800 text-4xl'>App</div>
+    <Paginator/>
+    </>
+    
   )
 }
