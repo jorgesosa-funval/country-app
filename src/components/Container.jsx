71e91837c9159be25e3cab90bfa72c6ab6adf05f
Jorge Sosa <jorgesosa@fundaciondevalores.org>
2025-01-21 15:41:07 -0400

    merge full project

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { Paginator } from './Paginator'
+import { Botones } from './Botones'
 export function Container() {
   return (
-    <div className='Container flex items-start justify-center bg-[#393F6E] rounded-lg w-96 h-[35rem] pt-44 lg:w-4/6 lg:h-[22rem] lg:pt-28'>
-        <h2 className='text-xl text-center text-white font-sans font-medium px-5 lg:px-44 lg:text-lg'>Wich country does this flag 🏁 belong to?</h2>
+    <div className='Container flex flex-col  bg-[#393F6E] rounded-lg w-96 h-[35rem]  lg:w-4/6 lg:h-[22rem] justify-between'>
+      <Paginator />
+
+      <h2 className='text-xl text-center text-white font-sans font-medium px-5 lg:px-44 lg:text-lg'>Wich country does this flag 🏁 belong to?</h2>
+
+      <Botones/>
+
     </div>
   )
 }
