7436368b72eb72d514d291c7e30f3ce28c2635c7
bohorquezbs <bsbm121@gmail.com>
2025-01-21 10:16:47 -0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Titulo from './components/Titulo'
+import Input from './components/Input'
 
 export default function App() {
   return (
-    <div className='bg-slate-800 text-4xl'>App</div>
+    <header className='flex bg-sky-950 justify-between items-center p-4'>
+      <Titulo />
+      <Input />
+    </header>
   )
 }
