770390ed78953ce4cc6b74cab6d5ecb5c7f794d5
@Omner3 <gonzalezomner@hotmail.com>
2025-01-21 08:56:24 -0600

    responsive container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Container } from "./components/Container";
 
 export default function App() {
   return (
-    <div className='bg-slate-800 text-4xl'>App</div>
+    <div className='flex flex-col items-center justify-center'>
+      <h1 className='pt-44 pb-5'>Country Quiz</h1>
+      <Container></Container>
+    </div>
   )
 }
---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Container() {
+  return (
+    <div className='Container flex items-start justify-center bg-[#393F6E] rounded-lg w-96 h-[34rem] pt-44 lg:w-4/6 lg:h-[22rem] lg:pt-28'>
+        <h2 className='text-xl text-center text-white font-sans font-medium px-10 lg:px-40'>Wich country does this flag 🏁 belong to?</h2>
+    </div>
+  )
+}
